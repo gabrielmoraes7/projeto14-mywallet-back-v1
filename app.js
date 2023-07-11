9537b61f1106c24d49e997019fa8cf3820527612
gabrielmoraes7 <gabriel.moclemente7@gmail.com>
2023-07-11 14:32:01 -0300

    conection DB

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import express from "express"
-import cors from "cors"
-import router from "./routes/index.routes.js"
+import dotenv from "dotenv"
 
 const app = express()
-
-app.use(cors())
 app.use(express.json())
-app.use(router)
+dotenv.config()
 
-const PORT = 4000
-app.listen(PORT, () => console.log(`Servidor está rodando na porta ${PORT}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
